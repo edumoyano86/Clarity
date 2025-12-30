20b32534ac7843f0c71f8d164d306d638ce44eba
Eduardo Moyano <cba2486@gmail.com>
2025-12-30 21:40:49 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,31 +1,46 @@
-// Shared config for both client and server.
+
 const sharedConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: ""
 };
 
-// Determine if we're on the server
 const isServer = typeof window === 'undefined';
 
-const getFirebaseConfig = () => {
-  if (isServer) {
-    // Server-side config
-    return {
-      ...sharedConfig,
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      appId: process.env.FIREBASE_APP_ID,
-    };
-  } else {
-    // Client-side config
-    return {
-      ...sharedConfig,
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    };
-  }
-};
+let firebaseConfig: any;
 
-export const firebaseConfig = getFirebaseConfig();
+if (isServer) {
+  // Server-side config
+  if (
+    !process.env.FIREBASE_API_KEY ||
+    !process.env.FIREBASE_AUTH_DOMAIN ||
+    !process.env.FIREBASE_PROJECT_ID
+  ) {
+    throw new Error('Firebase server environment variables are not set.');
+  }
+  firebaseConfig = {
+    ...sharedConfig,
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  };
+} else {
+  // Client-side config
+  if (
+    !process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
+    !process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
+    !process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+  ) {
+     throw new Error('Firebase client environment variables are not set.');
+  }
+  firebaseConfig = {
+    ...sharedConfig,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  };
+}
+
+export { firebaseConfig };
