c7b30ce60635aae9f0bb39e39e104a2dbeae1031
Eduardo Moyano <cba2486@gmail.com>
2025-12-30 21:51:18 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,71 +1,9 @@
-
 'use client';
 
-import React, { useRef, useState } from 'react';
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { SidebarTrigger } from "@/components/ui/sidebar";
+import dynamic from 'next/dynamic';
+
+const HeaderClient = dynamic(() => import('./header-client'), { ssr: false });
 
 export default function Header() {
-  const [avatarSrc, setAvatarSrc] = useState("https://picsum.photos/seed/user/40/40");
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const handleAvatarClick = () => {
-    fileInputRef.current?.click();
-  };
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        setAvatarSrc(e.target?.result as string);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
-  return (
-    <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur-sm sm:px-6">
-      <div className="md:hidden">
-        <SidebarTrigger />
-      </div>
-      <div className="flex-1">
-        {/* Can add breadcrumbs or page title here */}
-      </div>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Avatar className="cursor-pointer">
-            <AvatarImage src={avatarSrc} alt="Usuario" />
-            <AvatarFallback>U</AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>Mi Cuenta</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={handleAvatarClick}>
-            Cambiar Imagen
-          </DropdownMenuItem>
-          <DropdownMenuItem>Configuración</DropdownMenuItem>
-          <DropdownMenuItem>Soporte</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Cerrar Sesión</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <input
-        type="file"
-        ref={fileInputRef}
-        onChange={handleFileChange}
-        className="hidden"
-        accept="image/*"
-      />
-    </header>
-  );
+  return <HeaderClient />;
 }
