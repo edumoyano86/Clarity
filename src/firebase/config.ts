2d29c2f3ef66d79896eaa50cd0884ffbcaa34b9e
Eduardo Moyano <cba2486@gmail.com>
2025-12-31 00:29:46 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,46 +1,8 @@
-
-const sharedConfig = {
-  measurementId: ""
+export const firebaseConfig = {
+  "projectId": "studio-468089430-c9a2d",
+  "appId": "1:359313498136:web:671195af2ef3b9ef4aa36d",
+  "apiKey": "AIzaSyCWL9_jlU0amaMimjFIaqrsVfoM0i1GsIk",
+  "authDomain": "studio-468089430-c9a2d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "359313498136"
 };
-
-const isServer = typeof window === 'undefined';
-
-let firebaseConfig: any;
-
-if (isServer) {
-  // Server-side config
-  if (
-    !process.env.FIREBASE_API_KEY ||
-    !process.env.FIREBASE_AUTH_DOMAIN ||
-    !process.env.FIREBASE_PROJECT_ID
-  ) {
-    throw new Error('Firebase server environment variables are not set.');
-  }
-  firebaseConfig = {
-    ...sharedConfig,
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  };
-} else {
-  // Client-side config
-  if (
-    !process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
-    !process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
-    !process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
-  ) {
-     throw new Error('Firebase client environment variables are not set.');
-  }
-  firebaseConfig = {
-    ...sharedConfig,
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  };
-}
-
-export { firebaseConfig };
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,31 +1,50 @@
-import { initializeApp, getApps, type FirebaseApp } from 'firebase/app';
-import { getAuth, type Auth } from 'firebase/auth';
-import { getFirestore, type Firestore } from 'firebase/firestore';
-import { firebaseConfig } from './config';
+'use client';
 
-// Provides a singleton pattern for Firebase instances.
-// Ensures that we're not creating new instances on every render.
-let firebaseApp: FirebaseApp;
-let auth: Auth;
-let firestore: Firestore;
+import { firebaseConfig } from '@/firebase/config';
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
 
-function initializeFirebase() {
+// IMPORTANT: DO NOT MODIFY THIS FUNCTION
+export function initializeFirebase() {
   if (!getApps().length) {
-    firebaseApp = initializeApp(firebaseConfig);
-    auth = getAuth(firebaseApp);
-    firestore = getFirestore(firebaseApp);
+    // Important! initializeApp() is called without any arguments because Firebase App Hosting
+    // integrates with the initializeApp() function to provide the environment variables needed to
+    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
+    // without arguments.
+    let firebaseApp;
+    try {
+      // Attempt to initialize via Firebase App Hosting environment variables
+      firebaseApp = initializeApp();
+    } catch (e) {
+      // Only warn in production because it's normal to use the firebaseConfig to initialize
+      // during development
+      if (process.env.NODE_ENV === "production") {
+        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+      }
+      firebaseApp = initializeApp(firebaseConfig);
+    }
+
+    return getSdks(firebaseApp);
   }
-  return { firebaseApp, auth, firestore };
+
+  // If already initialized, return the SDKs with the already initialized App
+  return getSdks(getApp());
 }
 
-// Export the initialized services
-const services = initializeFirebase();
-firebaseApp = services.firebaseApp;
-auth = services.auth;
-firestore = services.firestore;
+export function getSdks(firebaseApp: FirebaseApp) {
+  return {
+    firebaseApp,
+    auth: getAuth(firebaseApp),
+    firestore: getFirestore(firebaseApp)
+  };
+}
 
-export { firebaseApp, auth, firestore, initializeFirebase };
 export * from './provider';
-export * from './auth/use-user';
+export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
+export * from './non-blocking-updates';
+export * from './non-blocking-login';
+export * from './errors';
+export * from './error-emitter';
