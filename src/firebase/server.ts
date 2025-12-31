d6d695f5803854cd54585a81e2b4d184155a0bbf
Eduardo Moyano <cba2486@gmail.com>
2025-12-31 00:18:08 +0000

    Try fixing this error: `Runtime Error: A "use server" file can only expo

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,5 +1,3 @@
-'use server'
-
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase-admin/app';
 import { getFirestore, type Firestore } from 'firebase-admin/firestore';
 import { credential } from 'firebase-admin';
