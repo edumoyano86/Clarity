af8a3a3f4559580a6d8224b03aaa63f82bb9ceaa
Eduardo Moyano <cba2486@gmail.com>
2026-01-02 16:42:59 +0000

    Set up a Firebase backend

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,20 +5,34 @@ import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
 
-// This function initializes Firebase and returns the SDKs.
+// IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  let firebaseApp: FirebaseApp;
-
-  // Check if Firebase has already been initialized.
   if (!getApps().length) {
-    // If not initialized, initialize with the provided config.
-    firebaseApp = initializeApp(firebaseConfig);
-  } else {
-    // If already initialized, get the existing app instance.
-    firebaseApp = getApp();
+    // Important! initializeApp() is called without any arguments because Firebase App Hosting
+    // integrates with the initializeApp() function to provide the environment variables needed to
+    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
+    // without arguments.
+    let firebaseApp;
+    try {
+      // Attempt to initialize via Firebase App Hosting environment variables
+      firebaseApp = initializeApp();
+    } catch (e) {
+      // Only warn in production because it's normal to use the firebaseConfig to initialize
+      // during development
+      if (process.env.NODE_ENV === "production") {
+        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+      }
+      firebaseApp = initializeApp(firebaseConfig);
+    }
+
+    return getSdks(firebaseApp);
   }
 
-  // Return the initialized app and the required SDKs.
+  // If already initialized, return the SDKs with the already initialized App
+  return getSdks(getApp());
+}
+
+export function getSdks(firebaseApp: FirebaseApp) {
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
