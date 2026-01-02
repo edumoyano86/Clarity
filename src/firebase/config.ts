755a68ecfe77fa338fae37697e8b6d276664467a
Eduardo Moyano <cba2486@gmail.com>
2026-01-02 15:33:25 +0000

    te paso para que agregues las claves y // Import the functions you need

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "clarity-d5e76",
-  "appId": "1:570928058130:web:be71618c75f203d3b7edcb",
   "apiKey": "AIzaSyD3S3kiDZuOVYBFUsBDoh7HtAEDts6sGKo",
   "authDomain": "clarity-d5e76.firebaseapp.com",
+  "projectId": "clarity-d5e76",
   "storageBucket": "clarity-d5e76.firebasestorage.app",
   "messagingSenderId": "570928058130",
+  "appId": "1:570928058130:web:be71618c75f203d3b7edcb",
   "measurementId": "G-8SSH26CXZH"
 };
