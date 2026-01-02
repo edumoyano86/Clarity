4ccb581b09e23cc8823539e41b840b83ba1b2cf1
Eduardo Moyano <cba2486@gmail.com>
2026-01-02 21:18:29 +0000

    el git push me queda asi conectado porque

--- src/firebase/client.ts
+++
@@ -1,5 +0,0 @@
-'use client';
-
-// This file is now simplified to re-export the single, correctly initialized instances.
-// This prevents any possibility of a duplicate or incorrect Firebase app instance.
-export { firebaseApp, auth, firestore } from './index';
