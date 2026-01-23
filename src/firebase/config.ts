016184a53c495d6c24bc3d22bcc36c8d3bba3d16
Eduardo Moyano <cba2486@gmail.com>
2026-01-23 01:20:02 +0000

    quiero hacer publico el github, para ponerlo en mi portfolio, pero como

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD3S3kiDZuOVYBFUsBDoh7HtAEDts6sGKo",
-  authDomain: "clarity-d5e76.firebaseapp.com",
-  projectId: "clarity-d5e76",
-  storageBucket: "clarity-d5e76.firebasestorage.app",
-  messagingSenderId: "570928058130",
-  appId: "1:570928058130:web:be71618c75f203d3b7edcb",
-  measurementId: "G-8SSH26CXZH"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
